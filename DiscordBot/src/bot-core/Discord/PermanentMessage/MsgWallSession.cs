40a0a17551b5462295103a5656161071a8f32407
Darxxemiyur Bayfaderix <darxxie@mail.bayfaderix.name>
2022-09-08 14:07:06 +0300

    DoMeMuMMem

--- DiscordBot/src/bot-core/Discord/PermanentMessage/MsgWallSession.cs
+++ DiscordBot/src/bot-core/Discord/PermanentMessage/MsgWallSession.cs
@@ -16,25 +16,23 @@ using Name.Bayfaderix.Darxxemiyur.Node.Network;
 
 namespace Manito.Discord.PermanentMessage
 {
-    public class MessageWallSession : DialogueNetSession
-    {
-        public readonly IPermMessageDbFactory DBFactory;
-        public MessageWallSession(InteractiveInteraction iargs,
-         MyDiscordClient client, DiscordUser user, IPermMessageDbFactory factory)
-         : base(iargs, client, user, false)
-        {
-            DBFactory = factory;
-        }
-        
-    }
-    public class MessageWallSessionController : DialogueNetSessionControls<MessageWallSession>
-    {
-        public MessageWallSessionController(MyDomain service) : base(service)
-        {
+	public class MessageWallSession : DialogueNetSession
+	{
+		public readonly IPermMessageDbFactory DBFactory;
+		public MessageWallSession(InteractiveInteraction iargs,
+		 MyDiscordClient client, DiscordUser user, IPermMessageDbFactory factory)
+		 : base(iargs, client, user, false) => DBFactory = factory;
 
-        }
-        public Task<MessageWallSession> StartSession(DiscordInteraction args,
-         Func<MessageWallSession, IDialogueNet> getNet) => StartSession(() =>
-         new MessageWallSession(new(args), Service.MyDiscordClient, args.User, Service.DbFactory), getNet);
-    }
+
+	}
+	public class MessageWallSessionController : DialogueNetSessionControls<MessageWallSession>
+	{
+		public MessageWallSessionController(MyDomain service) : base(service)
+		{
+
+		}
+		public Task<MessageWallSession> StartSession(DiscordInteraction args,
+		 Func<MessageWallSession, IDialogueNet> getNet) => StartSession(
+		 () => new MessageWallSession(new(args), Service.MyDiscordClient, args.User, Service.DbFactory), getNet);
+	}
 }
