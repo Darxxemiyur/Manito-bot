d4f45c3d348193a049f63c9621301656cfc771bc
Darxxemiyur Bayfaderix <darxxie@mail.bayfaderix.name>
2022-10-03 19:02:27 +0300

    WIP
    Signed-off-by: Darxxemiyur Bayfaderix <darxxie@mail.bayfaderix.name>

--- DiscordBot/src/bot-core/Discord/Shop/ShopItem.cs
+++ DiscordBot/src/bot-core/Discord/Shop/ShopItem.cs
@@ -3,28 +3,31 @@ using System.Collections;
 using DisCatSharp.Entities;
 using DisCatSharp.ApplicationCommands;
 using Microsoft.EntityFrameworkCore;
+using Manito.Discord.Inventory;
 
 namespace Manito.Discord.Shop
 {
 
-    public class ShopItem
-    {
-        /// <summary>
-        /// Name of the Item
-        /// </summary>
-        public string Name;
-
-
-        /// <summary>
-        /// Category of the Item
-        /// </summary>
-        public ShopItemCategory Category;
-        
-        
-        /// <summary>
-        /// Price for unit of Item
-        /// </summary>
-        public long Price;
-    }
-
+	public class ShopItem
+	{
+		/// <summary>
+		/// Name of the Item
+		/// </summary>
+		public string Name;
+		/// <summary>
+		/// Category of the Item
+		/// </summary>
+		public ShopItemCategory Category;
+		/// <summary>
+		/// Price for unit of Item
+		/// </summary>
+		public int Price;
+		public struct InCart
+		{
+			public readonly ShopItem Item;
+			public readonly int Amount;
+			public int Price => Item.Price * Amount;
+			public InCart(ShopItem item, int amount) => (Item, Amount) = (item, amount);
+		}
+	}
 }
